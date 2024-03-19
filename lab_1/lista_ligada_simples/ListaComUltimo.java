404d4885ddd88ac546fa7bca4c443b4bdc3f6427
TAYNAH TAMIE MASUNAGA SAITO <23.00621-8@maua.br>
2024-03-19 11:58:10 -0300

    decima aula - implementação do metodo removeFim

--- lab_1/lista_ligada_simples/ListaComUltimo.java
+++ lab_1/lista_ligada_simples/ListaComUltimo.java
@@ -13,4 +13,20 @@ public class ListaComUltimo extends Lista {
         ultimo = novo;
     }
     //exercicio: implementar os outros metodos (insereInicio, removeInicio, removeFim)
+    @Override
+    public int removeFim(){
+        int info = ultimo.getInfo();
+        if (super.getPrimeiro() == ultimo){
+            setPrimeiro(null);
+            ultimo = null;
+        }
+        else{
+            No temp = super.getPrimeiro();
+            while (temp.getProximo() != ultimo) {
+                temp = temp.getProximo();
+            }
+            temp.setProximo(null);
+        }
+        return info;
+    }
 }
